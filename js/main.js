7ed6290eb2bc2867c3f428781609ea66de3a8ae4
Konstantin Weitz <weitzkon@cs.uw.edu>
2014-09-28 07:14:19 -0700

    Scaling Header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 $('table').addClass('table well');
+
+var height = 250;
+// var width = 300;
+
+adjustHeader = function () {
+  var h = height- document.body.scrollTop;
+  $('#header').css('height', h);
+  $('#logo').css('height', h);
+
+//  var w = $(window).width() - width;
+//  $('#name').css('width', w);
+}
+
+
+window.onscroll = adjustHeader
+window.onresize = adjustHeader
+adjustHeader()
+
+$('body').css('padding-top', height);
