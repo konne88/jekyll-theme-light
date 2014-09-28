782c285ceb8d9e0cf01ccc8847d7a7404584d1ab
Konstantin Weitz <weitzkon@cs.uw.edu>
2014-09-28 09:32:18 -0700

    Everything working!

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 $('table').addClass('table well');
 
-var height = 250;
-// var width = 300;
+var height = parseInt($('#header').css('height'), 10)
+$('body').css('padding-top', height);
 
 adjustHeader = function () {
-  var h = height- document.body.scrollTop;
+  var h = height - document.body.scrollTop;
   $('#header').css('height', h);
-//  $('#logo').css('height', h);
-
-//  var w = $(window).width() - width;
-//  $('#name').css('width', w);
 }
 
-
 window.onscroll = adjustHeader
-// window.onresize = adjustHeader
 adjustHeader()
-
-$('body').css('padding-top', height);
